c8b626d5ceed367b6b032ac0eb48d1c2cfa0e2be
Michael Haas <haas@computerlinguist.org>
2015-02-25 14:52:31 +0100

    Add request handlers

---
+++ handlers.php
@@ -0,0 +1,155 @@
+<?php
+
+session_start();
+
+define('FACEBOOK_SDK_V4_SRC_DIR', '/include/facebook-php-sdk-v4/src/Facebook/');
+require '/include/facebook-php-sdk-v4/autoload.php';
+
+
+define('STAGE_LOGIN', 'login');
+define('STAGE_COUNTER', 'counters');
+
+define('AUTH_DENIED', '0');
+define('AUTH_ALLOWED', '1');
+define('AUTH_ERROR', '-1');
+
+
+// TODO: these must come from settings
+define('APP_ID', 'SOME_ID');
+define('APP_SECRET', 'SOME_SECRET');
+
+define('MY_URL', 'http://localhost/');
+
+define('PAGE_ID', '195413467191160');
+
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\FacebookRequestException;
+
+FacebookSession::setDefaultApplication('YOUR_APP_ID','YOUR_APP_SECRET');
+
+function handle_ping(){
+    return 'Pong';
+}
+
+function handle_fb_callback() {
+    $helper = new FacebookRedirectLoginHelper();
+    try {
+        $session = $helper->getSessionFromRedirect();
+    } catch(FacebookRequestException $ex) {
+      // When Facebook returns an error
+        Flight::error($ex);
+    } catch(\Exception $ex) {
+        // When validation fails or other local issues
+        Flight:error($ex);
+    }
+    if ($session) {
+        $_SESSION['FBTOKEN'] = $session->getToken();
+        // Render message form
+        Flight::render('')
+    }
+    else {
+        // Render error message
+    }
+    
+
+}
+
+function handle_checkin() {
+
+    $token = $_SESSION['FBTOKEN'];
+    if (empty($token)) {
+        Flight:error(new Exception('No FB token in session!'));
+    }
+    $message = Flight::request()->query->message;
+
+    $config = array(place => PLACE_ID);
+    if (! empty($message)) {
+        array['message'] = $message;
+    }
+
+    $request = new FacebookRequest(
+        $session,
+        'POST',
+        '/me/feed'
+        $config,
+    );
+
+    try {
+        $response = $request->execute();
+    } catch (FacebookRequestException $ex) {
+        Flight::error($ex);
+    } catch (\Exception $ex) {
+        Flight::error($ex);
+    }
+}
+    
+
+function fblogin() {
+
+    // Simplification: always assume we are not logged in!
+    $helper = new FacebookRedirectLoginHelper(MY_URL . 'fb_callback/');
+    $loginUrl = $helper->getLoginUrl();
+    Flight::render('fblogin', array('url' = > $loginUrl));
+
+
+}
+
+
+// User request
+function handle_login() {
+    //login/?gw_address=%s&gw_port=%d&gw_id=%s&url=%s 
+    $gw_address = $request->query->gw_address;
+    $gw_port = $request->query->gw_port;
+    $gw_id = $request->query->gw_id;
+    $req_url = $request->query->url;
+    $_SESSION['gw_address'] = $gw_address;
+    $_SESSION['gw_port'] = $gw_port;
+    $_SESSION['gw_id'] = $gw_id;
+    $_SESSION['req_url'] = $req_url;
+    fblogin();
+}
+
+// Gateway request
+function handle_auth() {
+    $request = Flight::request();
+    // stage=
+    //ip=
+    //mac=
+    //token=
+    //incoming=
+    //outgoing=
+    $gw_address = $request->query->stage;
+
+    if (empty($gw_address) || empty($gw_port) || empty($gw_id)) {
+        Flight::Error('stage parameter empty!');
+    }
+    if ($stage == STAGE_COUNTER) {
+        return;
+    }
+    // TODO: look up in global session? 
+}
+
+
+
+function write_auth_response($code) {
+    echo 'Auth: ' . $code;
+}
+
+
+function login_success() {
+    //  http://" . $gw_address . ":" . $gw_port . "/wifidog/auth?token=" . $token
+    // TODO: get token
+    $token = generate_token();
+    $_SESSION['gw_address'] = $gw_address;
+    $_SESSION['gw_port'] = $gw_port;
+    Flight::redirect('http://' . $_SESSION['gw_address'] . ':'
+        . $_SESSION['gw_port'] . '/wifidog/auth?token=' . $token);
+}
+
+function generate_token() {
+    // Taken from wifidog Token.php, but use sha1 instead of md5sum
+    return sha1(uniqid(rand(), 1));
+}
+
