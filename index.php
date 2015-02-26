0d4aeac226796dd561bb2c2f4b69688df87c812c
Michael Haas <haas@computerlinguist.org>
2015-02-26 13:32:47 +0100

    Set up DB, Enable secure sessions

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 
+// Configure some security features
+// These already have sane defaults in recent PHP versions,
+// so consider this as documentation
+
+ini_set('session.use_cookies', '1');
+ini_set('session.use_only_cookies', '1');
+ini_set('session.use_trans_sid', '0');
+// This does have a default value listed?
+ini_set('session.cookie_httponly', '1');
+// HTTPS only!
+ini_set('session.cookie_secure', '1');
+
 session_start();
 
 require_once('include/flight/Flight.php');
+
+// Sets up DB
+require_once('db.php');
 
 require_once('handlers/fb_handlers.php');
 
