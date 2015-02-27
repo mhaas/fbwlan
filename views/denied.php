c8a60c4a3fbdfe0bb6213bcba3dffabd0c9498d0
Michael Haas <haas@computerlinguist.org>
2015-02-27 11:41:27 +0100

    Re-indent code, fix retry url in views

--- views/denied.php
+++ views/denied.php
@@ -2,15 +2,16 @@
 
     <h2><?php echo _('That didn\'t work!') ?></h2>
     <div class="pure-g">
-       <div class="pure-u-1 pure-u-md-2-3">
-        <p> <?php
-            echo _('There was a problem:');
-            echo $msg;
-            ?>
-        </p>
+        <div class="pure-u-1 pure-u-md-2-3">
+            <p> <?php
+                echo _('There was a problem:');
+                echo ' ';
+                echo $msg;
+                ?>
+            </p>
         </div>
         <div class="pure-u-1 pure-u-md-1-3">
-            <a class="pure-button pure-button-primary" href="<?php echo $try_again_url; ?>"><?php echo _('Try again'); ?></a>
+            <a class="pure-button pure-button-primary" href="<?php echo $retry_url; ?>"><?php echo _('Try again'); ?></a>
         </div>
     </div>
 
