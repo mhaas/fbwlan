3faae549d35a4e500924a4224e646a2c8772864e
Michael Haas <haas@computerlinguist.org>
2015-02-28 10:18:39 +0100

    Access_code: fix form action

--- views/login.php
+++ views/login.php
@@ -31,11 +31,11 @@
         </div>
         <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
-                <form class="pure-form pure-form-stacked">
+                <form class="pure-form pure-form-stacked" action="<?php echo $codeurl; ?>">
                     <fieldset>
                         <legend><?php echo _('Access code'); ?></legend>
                         <input type="text" placeholder="XXXXXX" name="access_code"></input>
-                        <button type="submit" class="pure-button pure-button-primary" action="<?php echo $codeurl; ?>">
+                        <button type="submit" class="pure-button pure-button-primary">
                             <?php echo _('Sign in'); ?>
                         </button>
                     </fieldset>
