44e329174040deca4abbcbd0befac6c411637ad0
Michael Haas <haas@computerlinguist.org>
2015-02-28 10:41:17 +0100

    Fix h2 display on fb_callback

--- views/fb_callback.php
+++ views/fb_callback.php
@@ -1,6 +1,10 @@
 
 <?php echo $head; ?>
-    <h2><?php echo _('Facebook login successful!') ?></h2>
+    <div class="centered-row">
+        <div class="l-box">
+            <h2><?php echo _('Facebook login successful!') ?></h2>
+        </div>
+    </div>
     <div class="pure-g centered-row">
         <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
