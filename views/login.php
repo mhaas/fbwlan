3e51b87193b251c49f2e01cdc7e73f6e700d4663
Michael Haas <haas@computerlinguist.org>
2015-02-28 09:34:10 +0100

    Make form stacked (better alignment)

--- views/login.php
+++ views/login.php
@@ -32,7 +32,7 @@
         </div>
         <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
-                <form class="pure-form">
+                <form class="pure-form pure-form-stacked">
                     <fieldset>
                         <legend><?php echo _('Access code'); ?></legend>
                         <input type="text" placeholder="XXXXXX" name="access_code"></input>
