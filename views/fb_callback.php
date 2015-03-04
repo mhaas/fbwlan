a6066aa9792224be334754933ae9b5c9215dd0b1
Michael Haas <haas@computerlinguist.org>
2015-03-04 15:51:27 +0100

    Revamp FB msg form a bit
    Also make rest of the page responsive, grid-wise

--- views/fb_callback.php
+++ views/fb_callback.php
@@ -20,14 +20,12 @@
         <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
                 <p>
-                    <form class="pure-form" action="<?php echo $post_action; ?>">
+                    <form class="pure-form pure-form-stacked" action="<?php echo $post_action; ?>">
                         <fieldset>
                             <legend><?php echo _('Check In'); ?></legend>
-                            <textarea rows="2" name="message" id="text_fb_message" placeholder="<?php echo _('A message, if desired');?>"></textarea>
-                            <button class="pure-button" id="button_clear_fb_message">
-                                <?php echo _('Clear message'); ?>
-                            </button>
-                            <button type="submit" class="pure-button pure-button-primary" >
+                            <label for="text_fb_message"><?php echo _('Message'); ?></label>
+                            <textarea rows="2" name="message" id="text_fb_message" placeholder="<?php echo _('Optional');?>"></textarea>
+                            <button type="submit" class="pure-button pure-button-primary">
                                 <?php echo _('Check in to') . ' ' .$place_name; ?>
                             </button>
                         </fieldset>
@@ -37,15 +35,15 @@
         </div>
     </div>
     <div class="pure-g centered-row">
-        <div class="pure-u-1-2">
+        <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
                 <p> <?php echo _('If you do not want to check in, you can still use the access code!') ; ?> <p>
             </div>
         </div>
-        <div class="pure-u-1-2">
+        <div class="pure-u-1 pure-u-md-1-2">
             <div class="l-box">
                 <p>
-                    <a href="<?php echo $retry_url; ?>" class="pure-button"><?php echo _('Back to login'); ?></a>
+                    <a href="<?php echo $retry_url; ?>" class="pure-button pure-button-primary"><?php echo _('Back to login'); ?></a>
                 </p>
             </div>
         </div>
