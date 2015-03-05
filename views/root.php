d1f68f92446e314c2e0d9d3fbcdaea8d99232c05
Michael Haas <haas@computerlinguist.org>
2015-03-05 14:08:13 +0100

    Make instructions very clear on /
    This is helpful for the Facebook app review process

--- views/root.php
+++ views/root.php
@@ -1,6 +1,6 @@
 <?php echo $head; ?>
     <div class="pure-g centered-row">
-        <div class="pure-u-1 pure-u-md-1-2">
+        <div class="pure-u-1">
             <div class="l-box">
                 <p> <?php
                     echo _('Hey!');
@@ -8,13 +8,27 @@
                     ?>
                 </p>
                 <p> <?php
-                    echo _('There is nothing to see here.');
                     echo ' ';
-                    echo _('Would you like to visit us on Facebook?');
+                    echo _('Welcome to the captive portal.');
+                    ?>
+                </p>
+                <p> <?php
+                    echo ' ';
+                    echo _('You need to be connected to our WLAN Hotspot for complete functionality.');
+                    ?>
+                </p>
+                <p> <?php
+                    echo ' ';
+                    echo _('However, you can try a demo mode!');
+                    echo ' ';
+                    echo _('After you log in with Facebook, you will be redirected to our website at');
+                    echo ' ';
+                    echo $portal_url;
+                    echo '.';
                     ?>
                 </p>
                 <p>
-                    <a class="pure-button pure-button-primary" href="<?php echo $page_url; ?>"><?php echo $page_name . _(' on Facebook'); ?></a>
+                    <a class="pure-button pure-button-primary" href="<?php echo $page_url; ?>"><?php echo 'Demo: Connect to ' .  $page_name . _(' on Facebook'); ?></a>
                 </p>
             </div>
         </div>
